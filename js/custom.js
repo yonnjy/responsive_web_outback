bc20f8d971b59daec96ac9c4c50d58b2e537c90f
YoonJY <yoon_jaeyeong#naver.com>
2023-06-22 12:28:07 +0900

    230622

--- js/custom.js
+++ js/custom.js
@@ -13,7 +13,7 @@ $(function () {
     const MainSlide = new Swiper('.main_slide', {
         loop: true,
         parallax: true,
-        speed: 600,
+        speed: 1600,
         autoplay: {
             delay: 2500,
             disableOnInteraction: false,
@@ -29,16 +29,7 @@ $(function () {
         }
     });
 
-    const ItmSlide = new Swiper('.itm_slide', {
-        loop: true,
-        slidesPerView: 2,
-        spaceBetween: 400,
-        speed: 900,
-        autoplay: {
-            delay: 4000,
-            disableOnInteraction: false,
-        },
-    })
+
 
 
     $('.MainVisual .arrows .left').on('click', function () {
@@ -51,13 +42,27 @@ $(function () {
 
     $('.MainVisual .dots li').on('click', function () {
         const idx = $(this).index();
-        $(this).addClass('on').siblings().removeClass('on')
+        $(this).addClass('on').siblings().removeClass('on');
         MainSlide.slideTo(idx);
-    })
-    
-    // document.querySelector('MainItm .arrows .left').addEventListener('click', () => {
-    //     ItmSlide.slidPrev
-    // })
+    });
+
+
+    const ItmSlide = new Swiper('.itm_slide', {
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 0,
+        speed: 900,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 400,
+            }
+        },
+    });
 
     $('.MainItm .arrows .left').on('click', function () {
         ItmSlide.slidePrev();
@@ -66,5 +71,25 @@ $(function () {
         ItmSlide.slideNext();
     });
 
+    // document.querySelector('.MainItm .arrows .left').addEventListener('click', () => {
+    //     ItmSlide.slidePrev();
+    // });
 
-})
+    $('.mobile_btn').on('click', function () {
+        $(this).toggleClass('on');
+        $('.Gnb').toggleClass('on');
+    });
+
+    $('.Gnb>ul>li>a').on('click', function (e) {
+        e.preventDefault();
+        $(this).next().stop().slideDown();
+        $(this).parent().siblings().find('.snb').stop().slideUp();
+    });
+
+    // 모바일에서 서브눌렀다가 pc로 넘어와도 문제없게
+    $(window).on('resize', function(){
+        $('.Gnb .snb').removeAttr('style');
+    });
+    
+
+});
